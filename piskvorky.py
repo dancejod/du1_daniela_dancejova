9b6cb260e4bc5168f4f8e6eb745e106581a601b2
dancejod <dancejova.daniela@gmail.com>
2021-10-25 21:48:38 +0200

    Pridana funkcia na vykreslenie zadaneho poctu stlpcov. Riesene cez taznicu trojuholnika

--- piskvorky.py
+++ piskvorky.py
@@ -1,23 +1,31 @@
 from turtle import *
-from math import *
+from math import sqrt
 
 speed(1)
 dlzka = 20
 pocet_riadkov = 4
 pocet_stlpcov = 4
 
-###Vykresli zadany pocet riadkov
-for j in range(pocet_riadkov):
+### Vykresli zadany pocet stlpcov
+for k in range(pocet_stlpcov):
+
+### Vykresli zadany pocet riadkov
+    for j in range(pocet_riadkov):
     
-    ###Vykresli sestuholnik
-    for i in range(6):
+        ### Vykresli sestuholnik
+        for i in range(6):
+            forward(dlzka)
+            left(60)
+    
         forward(dlzka)
         left(60)
+        forward(dlzka)
+        right(60)
     
-    forward(dlzka)
-    left(60)
-    forward(dlzka)
-    right(60)
+    penup()
+    posun = sqrt((dlzka**2)-((dlzka/2)**2))     ## Taznica rovnostranneho trojuholnika
+    setpos(0,2*posun*(k+1))                     ## Posun y suradnice o dvojnasobok taznice krat poradie iteracie
+    pendown()
 
 
 exitonclick()
