b9ac7c596d5b31832aa151a32d95809076f57a35
dancejod <dancejova.daniela@gmail.com>
2021-10-25 21:10:22 +0200

    Pridana funkcia na vykreslenie zadaneho poctu riadkov

--- piskvorky.py
+++ piskvorky.py
@@ -1,9 +1,23 @@
 from turtle import *
+from math import *
 
+speed(1)
 dlzka = 20
-###Vykresli sestuholnik
-for i in range(6):
+pocet_riadkov = 4
+pocet_stlpcov = 4
+
+###Vykresli zadany pocet riadkov
+for j in range(pocet_riadkov):
+    
+    ###Vykresli sestuholnik
+    for i in range(6):
+        forward(dlzka)
+        left(60)
+    
     forward(dlzka)
     left(60)
+    forward(dlzka)
+    right(60)
+
 
 exitonclick()
