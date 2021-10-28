f5eae50f9202ef75f3060ce2d83b334093805577
dancejod <dancejova.daniela@gmail.com>
2021-10-28 21:00:06 +0200

    Pridane funkcne nastavovanie suradnic pre vykreslovanie symbolov

--- piskvorky.py
+++ piskvorky.py
@@ -1,5 +1,5 @@
 from turtle import *
-from math import sqrt
+from math import *
 
 speed(0)
 dlzka = 20
@@ -23,21 +23,46 @@ for k in range(pocet_riadkov):
         right(60)
     
     penup()
-    posun = sqrt((dlzka**2)-((dlzka/2)**2))     ## Taznica rovnostranneho trojuholnika
-    setpos(0,2*posun*(k+1))                     ## Posun y suradnice o dvojnasobok taznice krat poradie iteracie
+    taznica = sqrt((dlzka**2)-((dlzka/2)**2))     ## Taznica rovnostranneho trojuholnika
+    setpos(0,2*taznica*(k+1))                     ## taznica y suradnice o dvojnasobok taznice krat poradie iteracie
     pendown()
 
 penup()
-#setpos(dlzka/2,posun/2)
-setpos(dlzka/2, posun)
+setpos(0,0)
+
+#a, b = int(input("Hrac, zadaj policko vo formate [riadok, stlpec]"))
+
+pensize(5)
+
+riadok = 2
+stlpec = 3
+segment = (sqrt(dlzka**2-taznica**2))
+x_sur = (dlzka+segment)*(stlpec-1)
+y_sur = sin(taznica/dlzka)
+
+setx(x_sur)
+sety((y_sur/x_sur)*stlpec + (2*taznica*riadok))            ## taznicaie korytnacku do riadku
+#setpos((dlzka*b-dlzka,)
+
+### Krizik
+penup()
+#setpos(dlzka/2, taznica)
+pencolor("red")
+left(60)
+forward(10)
 pendown()
-left(60)
 forward(10)
 backward(20)
 forward(10)
 left(60)
 forward(10)
 backward(20)
-#circle(10)
+
+### Kruh
+penup()
+setpos(dlzka/2,taznica/2)
+pencolor("blue")
+pendown()
+circle(10)
 
 exitonclick()
