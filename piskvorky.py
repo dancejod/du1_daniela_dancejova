c9a844a69a49a39646754fef129961b3b20b684a
dancejod <dancejova.daniela@gmail.com>
2021-11-06 21:31:59 +0100

    Pridany kontrolny cyklus, upravene interakcie, piskvorky su hratelne

--- piskvorky.py
+++ piskvorky.py
@@ -1,10 +1,11 @@
 from turtle import *
-from math import sqrt, sin
+from math import sqrt
 
 speed(0)
-dlzka = 20
-pocet_stlpcov = int(input("Zadaj pocet stlpcov: "))
-pocet_riadkov = int(input("Zadaj pocet riadkov: "))
+dlzka_strany = 20
+pocet_stlpcov = int(input("Zadaj pocet stlpcov herneho pola: "))
+pocet_riadkov = int(input("Zadaj pocet riadkov herneho pola: "))
+vyska = sqrt((dlzka_strany**2)-((dlzka_strany/2)**2))           ## vyska 1 rovnostranneho trojuholnika v sestuholniku
 
 ### Vykresli zadany pocet riadkov
 for k in range(pocet_riadkov):
@@ -14,56 +15,81 @@ for k in range(pocet_riadkov):
     
         ### Vykresli sestuholnik
         for i in range(6):
-            forward(dlzka)
+            forward(dlzka_strany)
             left(60)
     
-        forward(dlzka)
+        forward(dlzka_strany)
         left(60)
-        forward(dlzka)
+        forward(dlzka_strany)
         right(60)
     
     penup()
-    vyska = sqrt((dlzka**2)-((dlzka/2)**2))     ##   vyska rovnostranneho trojuholnika, DAJ MIMO CYKLU
-    setpos(0,2*vyska*(k+1))                     ##   vyska y suradnice o dvojnasobok taznice krat poradie iteracie
+    setpos(0,2*vyska*(k+1))                     ##   posunie korytnacku na vykreslenie dalsieho riadku vyssie
     pendown()
 
 penup()
 setpos(0,0)
 
-#a, b = int(input("Hrac, zadaj policko vo formate [riadok, stlpec]"))
+### Krizik
+def krizik():
+    pencolor("red")
+    left(60)
+    pendown()
+    forward(10)
+    backward(20)
+    forward(10)
+    left(60)
+    forward(10)
+    backward(20)
+    right(120)
+    penup()
+
+### Kruh
+def kruh():
+    pencolor("blue")
+    pendown()
+    dot(25)
+    pencolor("white")
+    dot(18)
+    penup()
 
 pensize(5)
 
-hra = -1
+max_pocet_tahov = pocet_stlpcov * pocet_riadkov
 
-riadok = int(input("Zadaj cislo riadku pre tvoj tah: "))
-stlpec = int(input("Zadaj cislo stlpca pre tvoj tah: "))
-ls = []
-ls.append((riadok,stlpec))
-print(ls)
+### Hra
+for tah in range(max_pocet_tahov):
+    
+    if tah%2 == 1:
+        hrac = "Hrac 2 (cerveny)"
+    else:
+        hrac = "Hrac 1 (modry)"
 
-x_sur = ((stlpec-1)*((3/2)*dlzka))
-y_sur = ((stlpec-1)*vyska+(riadok-1)*2*vyska)
+    kontrola = 0
+    while kontrola == 0:                                    ### Kontrolny cyklus na vstupy, ak nie su splnene podmienky, hra nepokracuje
+    
+        riadok = int(input(hrac + ", zadaj cislo riadku pre tvoj tah: "))
+        if riadok <= 0 or riadok > pocet_riadkov:
+            print(hrac + ", zadal si neplatne cislo riadku")
+            continue
+        
+        stlpec = int(input(hrac + ", zadaj cislo stlpca pre tvoj tah: "))
+        if stlpec <= 0 or stlpec > pocet_stlpcov:
+            print(hrac + ", zadal si neplatne cislo stlpca")
+            continue
+            
+        kontrola = 1
+            
+    x_sur = ((stlpec-1)*((3/2)*dlzka_strany))
+    y_sur = ((stlpec-1)*vyska+(riadok-1)*2*vyska)
 
-setx(x_sur+dlzka/2)
-sety(y_sur+vyska)                           ## fkcia, ktora zasadi korytnacku do stredu sestuholnika
+    setx(x_sur+dlzka_strany/2)
+    sety(y_sur+vyska)                           ## korekcie zasadia korytnacku do stredu sestuholnika
 
-### Krizik
-penup()
-pencolor("red")
-left(60)
-pendown()
-forward(10)
-backward(20)
-forward(10)
-left(60)
-forward(10)
-backward(20)
-
-### Kruh
-penup()
-pencolor("blue")
-pendown()
-circle(10)
-
+    if tah%2 == 1:
+        krizik()
+    else:
+        kruh()
+        
+print("Koniec hry")
 exitonclick()
